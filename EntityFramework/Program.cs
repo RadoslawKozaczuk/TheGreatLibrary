68daaa80d1bcadaa76e7bc707d119f638f5b195e
RadoslawKozaczuk <radoslaw.kozaczuk@finalproject.pl>
2018-01-18 14:26:25 +0100

    EntityFramework - add data model

--- EntityFramework/Program.cs
+++ EntityFramework/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Reminder - to install DB:
+// - install SQL server (engine), it additionally contains SQL Management Studio
+// - add ADO .NET Entity Data Model to the project
+
 namespace EntityFramework
 {
 	class Program
