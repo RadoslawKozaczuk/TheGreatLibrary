6038508676cbc6f9331ee3aceec46b7f2da1a1e5
RadoslawKozaczuk <radoslaw.kozaczuk@finalproject.pl>
2018-01-25 11:23:59 +0100

    EntityFrameworkCore - few notes regarding annotations

--- EntityFrameworkCore/Model/Course.cs
+++ EntityFrameworkCore/Model/Course.cs
@@ -11,11 +11,18 @@ namespace EntityFrameworkCore.Model
 		Advanced = 3
 	}
 
+	// framework assumes all tables are in .dbo schema
+	//[Table("someTable", Schema = "mySchema")] // write this to map to another table or schema
 	class Course
 	{
-		[Key]
+		// by default primary key is ID or ClassNameID
+		// but Id is not cool man, that's illegal, you may go to jail for that
+		[Key] // bribe the policeman with this annotation
 		public int Id { get; set; }
+		// by default strings are mapped to nvarchar
+		//[Column("columnName", TypeName = "varchar")] // write this to map to another column or a different type
 		public string Name { get; set; }
+		[Required]
 		public string Description { get; set; }
 		public DateTime? DatePublished { get; set; }
 		public CourseLevel Level { get; set; }
