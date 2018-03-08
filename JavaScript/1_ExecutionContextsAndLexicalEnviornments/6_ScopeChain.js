981e2911996ac3dffff383d6bab47328ff510aa9
RadoslawKozaczuk <radoslaw.kozaczuk@finalproject.pl>
2018-03-08 10:25:05 +0100

    JavaScript - Asynchronous calls

--- JavaScript/1_ExecutionContextsAndLexicalEnviornments/6_ScopeChain.js
+++ JavaScript/1_ExecutionContextsAndLexicalEnviornments/6_ScopeChain.js
@@ -1,3 +1,6 @@
+// Scope
+//	Where a variable is available in your code. And it maybe the same variable or a new copy.
+
 // lexical environment make the difference
 // b sits in the file so it will have reference to the global object
 function b() {
@@ -39,3 +42,13 @@ function c() {
 var myVar2 = "global2";
 console.log("myVar2 = " + myVar2);
 c();
+
+
+// in ES6 there is also let keyword
+if(2 > 1) {
+	let c = true; // this variable is available only in the scope in was created in
+}
+
+for (i = 0; i < 10; i++) { 
+    let troll = 123; // this variable will be recreated in every cycle
+}
---
+++ JavaScript/1_ExecutionContextsAndLexicalEnviornments/7_AsynchronousCallbacks.js
@@ -0,0 +1,34 @@
+// Asynchronous - More than one at a time, 
+
+/*
+	there is a list inside JavaScript Engine named EventQueue
+	it holds things like HttpRequest or Click
+	when the execution stack is empty JavaScript Engine look in that list
+	lets sat there is a Click event
+	then Engine creates execution context for lets sat clickHandler function
+	then when that finishes Engine looks again on the list and so on
+*/
+
+// long running function
+function waitThreeSeconds() {
+    var ms = 3000 + new Date().getTime();
+    while (new Date() < ms){}
+    console.log("finished function");
+}
+
+function clickHandler() {
+    console.log("click event!");   
+}
+
+// listen for the click event
+document.addEventListener("click", clickHandler);
+
+waitThreeSeconds();
+console.log("finished execution");
+
+/*
+	no matter if we click or not first the function will finish then the global execution context
+	and Engine will process the event only at the moment when the execution stack is empty
+	so asynchronous calls are possible but only outside of the engine. Internally JavaScript process 
+	event in the order they came in and only after it finished processing current task
+*/
